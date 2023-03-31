36b2b5e30198cc0913cbbc31de859b80b6b9be85
Matt Zimmer <zimmermja@gmail.com>
2023-03-31 14:34:36 +0100

    initialised React with yarn; cleaned unnecessary files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import './index.css'
+
+function App() {
+  return (
+    <div className="container">
+      <button>Create an event</button>
+    </div>
+  );
+}
+
+export default App;
